1cfa7793bf32551deae3ec86f6ddf7f37ab8d7b0
FacuTello <facundostello@hotmail.com>
2023-03-20 20:37:34 -0300

    blackboard done and eraser all button

--- main.js
+++ main.js
@@ -2,59 +2,105 @@ const container = document.querySelector('.grid-container');
 
 
 for (let i = 1; i <= 256; i++) {
-    const item = document.createElement('div');
-    item.classList.add('grid-item');
-    container.appendChild(item);
-  };
+	const item = document.createElement('div');
+	item.classList.add('grid-item');
+	container.appendChild(item);
+};
 
-  const pixels = document.querySelectorAll('.grid-item');
-  isClick = "";
+const pixels = document.querySelectorAll('.grid-item');
+let isClick = "";
 
 
-   function mouseDown() {
-    // Agregar clase solo al elemento clickeado
-    this.classList.add('background-black');
-    isClick = true;
-  }
 
-  function mouseUp(){
-    if(isClick){
-      isClick = false;
-     // this.classList.remove('background-black');
-    }
-    
-  }
+function mouseDown() {
+	// Agregar clase solo al elemento clickeado
+	this.classList.add('background-black');
+	isClick = true;
+}
 
-  function mouseOut(){
-     if(isClick){
-      this.classList.add('background-black');
-    }
-  }
+function mouseUp() {
+	if (isClick) {
+		isClick = false;
+	}
 
-  function mouseMove(){
-    if(isClick){   
-    this.classList.add('background-black');
-  }else{
-    
-  }
-  }
+}
 
-  function Click(){
-    this.classList.add('background-black');
-  }
+function mouseOut() {
+	if (isClick) {
+		this.classList.add('background-black');
+	}
+}
 
-  
-  
-  for (let i = 0; i < pixels.length; i++) {
-    // Agregar evento click a cada elemento
-    pixels[i].addEventListener('mousedown', mouseDown);
-    pixels[i].addEventListener('mousemove', mouseMove);
-   //pixels[i].addEventListener('mouseover', mouseOver);
-    pixels[i].addEventListener('mouseup', mouseUp);
-    pixels[i].addEventListener('click', Click);
+function mouseMove() {
+	if (isClick) {
+		this.classList.add('background-black');
+	} else {
 
-    } 
-    
+	}
+}
+
+function click() {
+	this.classList.add('background-black');
+}
+
+function dblClick(){
+	this.classList.remove('backgorund-black');
+}
+
+for (let a = 0; a < pixels.length; a++) {
+	// Agregar evento click a cada elemento
+	pixels[a].addEventListener('mousedown', mouseDown);
+	pixels[a].addEventListener('mousemove', mouseMove);
+	pixels[a].addEventListener('mouseup', mouseUp);
+	pixels[a].addEventListener('click', click);
+	
+}
+
+reziseBtn = document.querySelector('#resize-Blackboard');
+
+function changeBlackboard() {
+	let number = prompt("¿Cuantos pixeles por lado necesita?", "");
+	pxTotal = 480 / parseInt(number);
+	container.style.gridTemplateColumns = "repeat(" + parseInt(number) + ", " + pxTotal + "px)";
+	container.style.gridTemplateRows = "repeat(" + parseInt(number) + ", " + pxTotal + "px)";
+	totalGridItems = number * number;
+	for (let x = 0; x < totalGridItems; x++) {
+		let newItem = document.createElement('div');
+		newItem.classList.add('new-grid-item');
+		container.appendChild(newItem);
+	};
+
+const newPixels = document.querySelectorAll('.new-grid-item');
+for (let y = 0; y < newPixels.length; y++) {
+	// Agregar evento click a cada elemento
+	newPixels[y].addEventListener('mousedown', mouseDown);
+	newPixels[y].addEventListener('mousemove', mouseMove);
+	newPixels[y].addEventListener('mouseup', mouseUp);
+	newPixels[y].addEventListener('click', click);
+	
+};
+};
+
+
+reziseBtn.addEventListener('click', changeBlackboard);
+
+function newBlackboard() {
+	for (let b = 0; b < pixels.length; b++) {
+		pixels[b].classList.remove('background-black');
+	};
+	const newPixels = document.querySelectorAll('.new-grid-item');
+	for (let y = 0; y < newPixels.length; y++) {
+		// Agregar evento click a cada elemento
+		newPixels[y].classList.remove('background-black');
+	};
+
+};
+
+btnEraserAll = document.querySelector('#eraserAll');
+btnEraserAll.addEventListener('click', newBlackboard);
+
+
+
 
     
     
